c96f06a509bb3ece04a056fe207951b44a514fdc
Pavel Chen <pchen@ozon.ru>
2023-12-26 03:34:31 +0300

    sorted teaching load

--- internal/handler/student_handler/get_teaching_load.go
+++ internal/handler/student_handler/get_teaching_load.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"uir_draft/internal/pkg/service/student/mapping"
 )
 
 func (h *studentHandler) GetTeachingLoad(ctx *gin.Context) {
@@ -20,20 +19,5 @@ func (h *studentHandler) GetTeachingLoad(ctx *gin.Context) {
 		return
 	}
 
-	arr := &mapping.SemesterTeachingLoad{}
-
-	for _, el := range load.Array {
-		switch {
-		case el.Semester == int32(1):
-			arr.Semester1 = append(arr.Semester1, el)
-		case el.Semester == int32(2):
-			arr.Semester2 = append(arr.Semester2, el)
-		case el.Semester == int32(3):
-			arr.Semester3 = append(arr.Semester3, el)
-		case el.Semester == int32(4):
-			arr.Semester4 = append(arr.Semester4, el)
-		}
-	}
-
-	ctx.JSON(http.StatusOK, arr)
+	ctx.JSON(http.StatusOK, load)
 }
