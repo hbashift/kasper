e33a569bf777f2b3099efb9a0c0aae2b19fa4353
Pavel Chen <pchen@ozon.ru>
2024-01-02 14:25:52 +0300

    download dissertation

--- internal/app/kasper/service.go
+++ internal/app/kasper/service.go
@@ -35,8 +35,8 @@ func InitRoutes(student StudentHandler, supervisor SupervisorHandler, authorizat
 
 	router.Use(cors.New(cors.Config{
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type", "Accept-Encoding", "StudentID", "File-Name"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "StudentID", "File-Name"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type", "Accept-Encoding", "StudentID"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "Content-Disposition"},
 		AllowCredentials: true,
 		AllowAllOrigins:  true,
 		MaxAge:           12 * time.Hour,
