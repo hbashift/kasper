7efd3659f317267ed62c0bdb8620db4bdcb6475d
Pavel Chen <pchen@ozon.ru>
2024-04-16 20:53:58 +0300

    fix individual load type scan

--- internal/pkg/models/mapping.go
+++ internal/pkg/models/mapping.go
@@ -204,7 +204,7 @@ func MapIndividualWorkToDomain(loads []IndividualStudentsLoad, tLoadID uuid.UUID
 			Comment:        load.Comment,
 		}
 
-		var loadType model.ApprovalStatus
+		var loadType model.IndividualStudentsLoadType
 		if err = loadType.Scan(load.LoadType); err != nil {
 			return nil, nil, errors.Wrap(err, "MapIndividualWorkToDomain()")
 		}
